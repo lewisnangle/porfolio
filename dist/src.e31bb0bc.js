df22701520ee10fa56a2b188d6924d5f4486f0a2
Lewis Nangle <lewisnangle1@gmail.com>
2019-12-04 16:04:38 +0000

    refactored social profiles to be stateless functional component

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -24361,83 +24361,31 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-var SocialProfile =
-/*#__PURE__*/
-function (_Component) {
-  _inherits(SocialProfile, _Component);
-
-  function SocialProfile() {
-    _classCallCheck(this, SocialProfile);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(SocialProfile).apply(this, arguments));
-  }
-
-  _createClass(SocialProfile, [{
-    key: "render",
-    value: function render() {
-      var _this$props$socialPro = this.props.socialProfile,
-          link = _this$props$socialPro.link,
-          image = _this$props$socialPro.image;
-      return _react.default.createElement("span", null, _react.default.createElement("a", {
-        href: link
-      }, _react.default.createElement("img", {
-        src: image,
-        alt: "social-profile",
-        style: {
-          width: 35,
-          height: 35,
-          margin: 5
-        }
-      })));
+var SocialProfile = function SocialProfile(props) {
+  var _props$socialProfile = props.socialProfile,
+      link = _props$socialProfile.link,
+      image = _props$socialProfile.image;
+  return _react.default.createElement("span", null, _react.default.createElement("a", {
+    href: link
+  }, _react.default.createElement("img", {
+    src: image,
+    alt: "social-profile",
+    style: {
+      width: 35,
+      height: 35,
+      margin: 5
     }
-  }]);
+  })));
+};
 
-  return SocialProfile;
-}(_react.Component);
-
-var SocialProfiles =
-/*#__PURE__*/
-function (_Component2) {
-  _inherits(SocialProfiles, _Component2);
-
-  function SocialProfiles() {
-    _classCallCheck(this, SocialProfiles);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(SocialProfiles).apply(this, arguments));
-  }
-
-  _createClass(SocialProfiles, [{
-    key: "render",
-    value: function render() {
-      return _react.default.createElement("div", null, _react.default.createElement("h2", null, "Social Media Profiles"), _react.default.createElement("div", null, _socialProfiles.default.map(function (SOCIAL_PROFILE) {
-        return _react.default.createElement(SocialProfile, {
-          key: SOCIAL_PROFILE.id,
-          socialProfile: SOCIAL_PROFILE
-        });
-      })));
-    }
-  }]);
-
-  return SocialProfiles;
-}(_react.Component);
+var SocialProfiles = function SocialProfiles() {
+  return _react.default.createElement("div", null, _react.default.createElement("h2", null, "Social Media Profiles"), _react.default.createElement("div", null, _socialProfiles.default.map(function (SOCIAL_PROFILE) {
+    return _react.default.createElement(SocialProfile, {
+      key: SOCIAL_PROFILE.id,
+      socialProfile: SOCIAL_PROFILE
+    });
+  })));
+};
 
 var _default = SocialProfiles;
 exports.default = _default;
@@ -24586,7 +24534,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56704" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62342" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
